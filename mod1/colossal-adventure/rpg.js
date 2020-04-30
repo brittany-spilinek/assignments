81fe87027f6526b978fc946e78d29ae63d2bea8b
Brittany Spilinek <brittany.spilinek@gmail.com>
2020-04-30 10:58:20 -0600

    Second, Module 1 complete

---
+++ mod1/colossal-adventure/rpg.js
@@ -0,0 +1,115 @@
+const readline = require("readline-sync");
+
+//*****Variables*****//
+let gameOver = false
+
+function character (name, health, damage, attack, items, phrase) {
+    this.name = name;
+    this.health = health;
+    this.damage = damage;
+    this.attack = attack;
+    this.items = items;
+    this.phrase = phrase;
+}
+
+const player = new character ("", 100, 40, "zaps")
+const infected = new character ("A COVID-19-Infected Person", 80, 20, " coughs on", [" an uncashed stimulus check"], "Ahh man, now I'm *cough* *hack* late for my party!")
+const trump = new character ("Trump, armed with Lysol and misinformation,", 60, 15, " pitifully swipes at", [" a Lysol-filled syringe"], "FLATTENING OF THE CURVE @realDonaldTrump")
+const karen = new character ("Karen, a maskless protestor,", 40, 10, " screeches at", [" several cases of toilet paper"], "You got blood on my jeans. I'm reporting this to your manager!!")
+const conspiracy = new character ("Coronavirus Conspiracy Theorist", 20, 5, " agressively argues with", [" a crusty tin-foil hat"], "THOSE 5G TOWERS MADE ME DO THIS!!")
+
+let enemiesArray = [infected, trump, karen, conspiracy]
+
+let inventory = [" a 6-foot cattle prod", " and a homemade mask"]
+
+//*****Game Functions*****//
+
+function walk () { 
+    console.log("You walk forward.")
+    const fate = Math.floor(Math.random() * 4) + 1 
+    if (fate != 1) {
+        return options
+    } else {
+        return randomEnemy()
+    } 
+}
+const playerFight = function (player) {
+    return Math.floor(Math.random() * player.damage) + 1
+}
+const enemyFight = function (character) {
+    return Math.floor(Math.random() * character.damage) + 1
+} 
+
+function randomEnemy () {
+    let num = Math.floor(Math.random() * enemiesArray.length) //determines which enemy will appear
+    const escapeMath = Math.floor(Math.random() * 2) + 1
+    readline.question(enemiesArray[num].name + " has appeared and is invading your personal space!")
+        fightOrFlight = readline.keyIn("Do you [A] attack or try to [F] flee? ", {limit: 'af'});
+        if (fightOrFlight.toLowerCase() === 'f') {
+            if (escapeMath === 1) {
+                let fleeDamage = enemyFight(enemiesArray[num]);
+                player.health = (player.health - fleeDamage)
+                readline.question("You have escaped, but took " + fleeDamage + " damage.")
+            } else {
+                readline.question("You want to flee... but you're far tnodeoo slow.")
+                fight ()
+            }
+        } else if (fightOrFlight.toLowerCase() === 'a') {
+            readline.question("You blindly hurl yourself into danger.")
+            fight()
+        } 
+    function fight () {
+        while (player.health > 0, enemiesArray[num].health > 0) {
+            for (let i = 0; i < 1; i++) {
+                let damage = playerFight(player)
+                enemiesArray[num].health = (enemiesArray[num].health - damage)
+                readline.question(player.name + " zaps the enemy with the cattle prod for " + damage + " damage.")
+            } 
+            for (let i = 0; i < 1; i++) {
+                let damage = enemyFight(enemiesArray[num])
+                player.health = (player.health - damage)
+                readline.question(enemiesArray[num].name + enemiesArray[num].attack + " you for " + damage + " damage.")
+            }
+        } 
+        if (player.health <= 0) {
+            readline.question(enemiesArray[num].phrase)
+            console.log("You have died. Does this count as a COVID-19 death? No?")
+            gameOver = true
+        } else if (enemiesArray[num].health <= 0) {
+            inventory.unshift(enemiesArray[num].items);
+            enemiesArray.splice(num, 1);
+            let someHealth = Math.floor(Math.random() * 10) + 1
+            player.health = (player.health + someHealth)
+            readline.question("You won" + inventory[0] + " and " + someHealth + " extra health!")
+            if (enemiesArray.length > 0) {
+                return options
+            } else {
+                console.log("You have narrowly escaped with your life and made it to... the line wrapping around the store. Screw it. You're lactose intolerant anyway.")
+                gameOver = true
+            }
+        } 
+    }
+}
+
+//*****GAME*****//
+console.log("It is the year 2020. As it turns out, the Mayans were right! They were just off by 8 years.");
+player.name = readline.question("What is your name? ");
+readline.question("It may be the end of times, " + player.name + ", but you are out of milk. Put on your mask, grab your cattle prod (I don't know why you have that, either) and let's take a trip to the grocery store!");
+
+
+while (!gameOver) {
+
+options = readline.keyIn("[W] to walk forward, [P] to see your health and inventory, [Q] to quit. ", {limit: 'wpq'});
+    if (options.toLowerCase() === 'w') {
+        walk()
+
+    } else if (options.toLowerCase() === 'p') {
+        console.log("Hello " + player.name + ". Your health is " + player.health + ". You have" + inventory)
+
+    } else if (options.toLowerCase() === 'q') {
+        console.log("Ok bye!"); 
+        {break;}
+    }
+
+}
+
--- mod1/problem-solving/problem-solving.js
+++ mod1/problem-solving/problem-solving.js
@@ -11,3 +11,32 @@ console.log (findmax())
 //  "!" => ["C%4!", "Hey!"]
 
 const lettersWithStrings = ["#3", "$$$", "C%4!", "Hey!"]
+const character = "!"
+const newArr = []
+
+function findwords (lettersWithStrings, character) {
+    for (let i = 0; i < lettersWithStrings.length; i++) {
+        if (lettersWithStrings[i].includes(character) === true) {
+            newArr.push(lettersWithStrings[i])
+        }
+    }  
+    return newArr    
+}
+console.log(findwords(lettersWithStrings,character))
+
+//3. Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2.
+//isDivisible(4, 2) // => true
+//isDivisible(9, 3) // => true
+//sDivisible(15, 4) // => false
+
+const num1 = 15
+const num2 = 4
+
+function isDivisible (num1, num2) {
+    if (num1 % num2 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(isDivisible(num1, num2))
